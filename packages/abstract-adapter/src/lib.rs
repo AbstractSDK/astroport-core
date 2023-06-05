5c2e4fa45915e80bde126927b7938e289e180de6
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-05 11:55:40 +0200

    Restructured abstract-adapter package

--- packages/abstract-adapter/src/lib.rs
+++ packages/abstract-adapter/src/lib.rs
@@ -1,6 +1,2 @@
-
 pub mod dex;
-pub use dex::{Astroport as AstroportDex, ASTROPORT as ASTROPORT_DEX};
-
 pub mod staking;
-pub use staking::{Astroport as AstroportStaking, ASTROPORT as ASTROPORT_STAKING};
