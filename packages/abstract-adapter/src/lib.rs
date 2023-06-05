c36878cff8abadf7c539ef5dca5f14cd51ef6bd1
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-05 10:48:12 +0200

    Added staking adapter

---
+++ packages/abstract-adapter/src/lib.rs
@@ -0,0 +1,6 @@
+
+pub mod dex;
+pub use dex::{Astroport as AstroportDex, ASTROPORT as ASTROPORT_DEX};
+
+pub mod staking;
+pub use staking::{Astroport as AstroportStaking, ASTROPORT as ASTROPORT_STAKING};
