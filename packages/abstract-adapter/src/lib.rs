04728f2dca0b64b9d716c447c1bb901651acc88b
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-09 10:46:35 +0200

    Added local testnet chain

--- packages/abstract-adapter/src/lib.rs
+++ packages/abstract-adapter/src/lib.rs
@@ -1,5 +1,5 @@
 pub const ASTROPORT: &str = "astroport";
-pub const AVAILABLE_CHAINS: &[&str] = &["terra", "injective"];
+pub const AVAILABLE_CHAINS: &[&str] = &["terra", "injective","cosmos-testnet"];
 
 pub mod dex;
 pub mod staking;
